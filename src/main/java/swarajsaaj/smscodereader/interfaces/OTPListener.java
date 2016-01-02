7801182dbd1e143c1ece5e61d79fbaa07e3c9bf2
unknown <swarajpal@SWARAJ3814.Nagarro.local>
2016-01-03 00:43:49 +0530

    First commit.

---
+++ src/main/java/swarajsaaj/smscodereader/interfaces/OTPListener.java
@@ -0,0 +1,9 @@
+package swarajsaaj.smscodereader.interfaces;
+
+/**
+ * Created by swarajpal on 13-12-2015.
+ */
+public interface OTPListener {
+
+    public void otpReceived(String messageText);
+}
---
+++ src/main/java/swarajsaaj/smscodereader/receivers/OtpReader.java
@@ -0,0 +1,68 @@
+package swarajsaaj.smscodereader.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+import swarajsaaj.smscodereader.interfaces.OTPListener;
+
+
+/**
+ * Created by swarajpal on 13-12-2015.
+ * BroadcastReceiver OtpReader for receiving and processing the SMS messages.
+ */
+public class OtpReader extends BroadcastReceiver {
+
+    /**
+     * Constant TAG for logging key.
+     */
+    private static final String TAG = "OtpReader";
+
+    /**
+     * The bound OTP Listener that will be trigerred on receiving message.
+     */
+    private static OTPListener otpListener;
+
+    /**
+     * The Sender number string.
+     */
+    private static String receiverString;
+
+    /**
+     * Binds the sender string and listener for callback.
+     *
+     * @param listener
+     * @param sender
+     */
+    public static void bind(OTPListener listener, String sender) {
+        otpListener = listener;
+        receiverString = sender;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+
+                final Object[] pdusArr = (Object[]) bundle.get("pdus");
+
+                for (int i = 0; i < pdusArr.length; i++) {
+
+                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusArr[i]);
+                    String senderNum = currentMessage.getDisplayOriginatingAddress();
+                    String message = currentMessage.getDisplayMessageBody();
+                    Log.i(TAG, "senderNum: " + senderNum + " message: " + message);
+
+                    if (senderNum.contains(receiverString)) { //If message received is from required number.
+                        //If bound a listener interface, callback the overriden method.
+                        if (otpListener != null) {
+                            otpListener.otpReceived(message);
+                        }
+                    }
+                }
+            }
+    }
+}
